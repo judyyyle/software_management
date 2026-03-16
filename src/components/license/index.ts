69408cf3a6f2cb3893a46bbd8f0eff9ee88dbc33
Nicole_zcr <3380782523@qq.com>
2026-03-16 15:18:11 +0800

    Initial commit

---
+++ src/components/license/index.ts
@@ -0,0 +1,11 @@
+import LicenseGuard from './LicenseGuard.vue'
+import LicenseStatus from './LicenseStatus.vue'
+import LicenseActivate from './LicenseActivate.vue'
+
+export { LicenseGuard, LicenseStatus, LicenseActivate }
+
+export default {
+    LicenseGuard,
+    LicenseStatus,
+    LicenseActivate,
+}
---
+++ src/lang/backend/zh-cn/firmware.ts
@@ -0,0 +1,5 @@
+export default {
+    firmware: '固件管理',
+    'firmware/version': '固件版本',
+    'firmware/upgrade': '升级任务',
+}
